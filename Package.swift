b746186ddf6aad99a10b0363847752e6892b98ec
Andrei Popescu <andproff@gmail.com>
2026-02-26 14:51:14 +0200

    Fix ObjectMapper URL format in Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
 
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.2"),
-        .package(url: "https://github.com:tristanhimmelman/ObjectMapper", from: "4.2.0")
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper", from: "4.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
